4111a3c2307ac1e16a05307751dcf914f2fd8b97
JacobGoelz <42621191+JacobGoelz@users.noreply.github.com>
2020-09-04 18:49:09 -0400

    Completed toRoman and fromRoman

--- romanconvertor.rb
+++ romanconvertor.rb
@@ -1,9 +1,29 @@
 def fromRoman(romanNumber)
-    # Replace the following line with the actual code!
+    return result if str.empty?
+    roman_mapping.values.each do |roman|
+      if str.start_with?(roman)
+        result += roman_mapping.invert[roman]
+        str = str.slice(roman.length, str.length)
+        return toRoman(str, result)
+      end
+    end
+  end
     raise NotImplementedError
 end
 
 def toRoman(arabicNumber)
-    # Replace the following line with the actual code!
+    integer = self
+    roman = ""
+
+    while integer > 0
+      if @@values[integer]
+        roman += @@values[integer]
+        return roman
+      end
+
+      roman += @@values[next_lower_key(integer)] 
+      integer -= next_lower_key(integer) 
+    end
+  end
     raise NotImplementedError
 end
